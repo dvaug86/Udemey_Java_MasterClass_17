a0f50c2f40935cb003101f2ba3756ef8f809f5ae
David Vaughan <dvaug86@gmail.com>
2024-05-22 08:28:24 -0400

    finished intro lesson for Hello World

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello, Tim"); //use println to put the next line of code on the next line
+        System.out.print("hello, World");
+    }
+}
