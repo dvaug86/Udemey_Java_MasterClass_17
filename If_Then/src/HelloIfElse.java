105158d0dc1ab905c33f346dfc00602e3b3f6dcc
David Vaughan <dvaug86@gmail.com>
2024-06-07 17:59:36 -0400

    using logical Or  operator

--- If_Then/src/HelloIfElse.java
+++ If_Then/src/HelloIfElse.java
@@ -9,13 +9,17 @@ public class HelloIfElse {
         }
 
         int topScore = 80;
-        if(topScore >= 100){
+        if (topScore >= 100) {
             System.out.println("You got the High score!");
         }
 
         int secondTopScore = 60;
-        if(topScore> secondTopScore && secondTopScore < 100){
+        if ((topScore > secondTopScore) && (secondTopScore < 100)) {
             System.out.println("Greater than second top score and less than 100 ");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
         }
     }
 
