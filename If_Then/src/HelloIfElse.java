4b11ab59c74b8339c1c07d00ab7c55696dd0b5bd
David Vaughan <dvaug86@gmail.com>
2024-06-07 15:40:01 -0400

    start of if-else statments

---
+++ If_Then/src/HelloIfElse.java
@@ -0,0 +1,12 @@
+public class HelloIfElse {
+    public static void main(String[] args) {
+        System.out.println("Hello, Tim");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("He is not an alien");
+            System.out.println("And I am scared of aliens");
+        }
+    }
+
+}
