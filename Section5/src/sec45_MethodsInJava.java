e679f5b38082626f53218df974c701b8edca8ecd
David Vaughan <dvaug86@gmail.com>
2024-06-09 09:51:09 -0400

    ran out of time before work. will finish this evening

--- Section5/src/sec45_MethodsInJava.java
+++ Section5/src/sec45_MethodsInJava.java
@@ -1,13 +1,13 @@
-public class sec45_MethodsInJava {
-    public static void main(String[] args) {
+// public class sec45_MethodsInJava {
+//     public static void main(String[] args) {
 
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+        // boolean gameOver = true;
+        // int score = 800;
+        // int levelCompleted = 5;
+        // int bonus = 100;
 
-        // int finalScore = score;
-        calculateScore( true, 800, levelCompleted, bonus);
+        // // int finalScore = score;
+        // calculateScore( true, 800, levelCompleted, bonus);
        
        
         // new paramters
@@ -23,19 +23,41 @@ public class sec45_MethodsInJava {
         //     System.out.println("Your final Score was " + finalScore);
         // }
 
-    }
+    // }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    // public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         // boolean gameOver = true;
         // int score = 800;
         // int levelCompleted = 5;
         // int bonus = 100;
 
-        int finalScore = score;
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            finalScore +=1000;
-            System.out.println("Your final Score was " + finalScore);
-        }
+    //     int finalScore = score;
+    //     if (gameOver) {
+    //         finalScore += (levelCompleted * bonus);
+    //         finalScore +=1000;
+    //         System.out.println("Your final Score was " + finalScore);
+    //     }
+    // }
+
+/////cleaning the code up above down below
+
+public class sec45_MethodsInJava {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+calculateScore(true, 800, levelCompleted, bonus); //should delete the variables above and enter the values here to simplify code, just want to show this as an example 
+calculateScore(true, 10000, 8, 200);
     }
+public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    int finalScore = score;
+    if (gameOver) {
+        finalScore += (levelCompleted * bonus);
+        finalScore +=1000;
+        System.out.println("Your final Score was " + finalScore);
+    }
+}
 }
