56b59795cbbb6ab9a4579ad9e5c654a6c69c635a
David Vaughan <dvaug86@gmail.com>
2024-06-13 14:18:05 -0400

    slimmed down the code

--- Section5/src/sec46_MethodChallange.java
+++ Section5/src/sec46_MethodChallange.java
@@ -28,7 +28,7 @@ public class sec46_MethodChallange {
     }
 
     public static int calculateHighScorePosition(int playerScore) {
-int position = 4
+int position = 4;
         if (playerScore > 1000) {
             position= 1;
         } else if (playerScore >= 500) { // original line: } else if (playerScore >= 500 && playerScore < 1000) {
