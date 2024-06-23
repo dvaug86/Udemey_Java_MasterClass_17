780059abccc7fd86bcbb73588d483dce0b79e406
David Vaughan <dvaug86@gmail.com>
2024-06-23 08:47:38 -0400

    failed attempt 2, will finish after work

--- Section5/src/CodingExercise4_BarkingDog.java
+++ Section5/src/CodingExercise4_BarkingDog.java
@@ -1,17 +1,30 @@
 public class CodingExercise4_BarkingDog {
     public static void main(String[] args) {
-        shouldWakeUp(true,1);
-        shouldWakeUp(false,2);
-        shouldWakeUp(true,8);
-        shouldWakeUp(true,-1);
+        shouldWakeUp(true, 1);
+        shouldWakeUp(false, 2);
+        shouldWakeUp(true, 8);
+        shouldWakeUp(true, -1);
+        
     }
-    public static void shouldWakeUp(boolean barking, int hourOfDay){
-       boolean wakeUp = true;
-        if((barking = true) && ((hourOfDay >= 8) || (hourOfDay <= 22) || (hourOfDay <0) || (hourOfDay >23))){
+
+    public static void shouldWakeUp(boolean barking, int hourOfDay) {
+        boolean wakeUp = true;
+        if ((hourOfDay < 0) || (hourOfDay > 23)) {
             wakeUp = false;
-            System.out.println(wakeUp);
+        } else if (barking = true && ((hourOfDay >= 8) && (hourOfDay <= 22))) {
+            wakeUp = false;
         }
         System.out.println(wakeUp);
     }
-    
+
 }
+
+// failed attempt 1
+// public static void shouldWakeUp(boolean barking, int hourOfDay){
+// boolean wakeUp = true;
+// if((barking = true) && ((hourOfDay >= 8) || (hourOfDay <= 22) || (hourOfDay
+// <0) || (hourOfDay >23))){
+// wakeUp = false;
+// System.out.println(wakeUp);
+// }
+// System.out.println(wakeUp);
