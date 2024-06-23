9e185b2ebd0ad059f3703cdb9b86a1515a8dc770
David Vaughan <dvaug86@gmail.com>
2024-06-23 07:35:32 -0400

    syntatcially right answer

--- Section5/src/CodingExercise2_SpeedConverter.java
+++ Section5/src/CodingExercise2_SpeedConverter.java
@@ -5,31 +5,60 @@ If the parameter kilometersPerHour is less than 0, the method toMilesPerHour nee
 
 Otherwise, if it is positive, calculate the value of miles per hour, round it and return it. For conversion and rounding, check the notes in the text below.
  * 
+ * 
+ * 2. Write another method called printConversion with 1 parameter of type double with the name kilometersPerHour.
+
+This method should not return anything (void) and it needs to calculate milesPerHour from the kilometersPerHour parameter.
+
+Then it needs to print a message in the format "XX km/h = YY mi/h".
+
+XX represents the original value kilometersPerHour.
+YY represents the rounded milesPerHour from the kilometersPerHour parameter.
+
+If the parameter kilometersPerHour is < 0 then print the text "Invalid Value".
  */
 
 public class CodingExercise2_SpeedConverter {
 
-    public static long toMilesPerHour(double kilometersPerHour){
+    // public static long toMilesPerHour(double kilometersPerHour){
 
-        if (kilometersPerHour <0) {
+    //     if (kilometersPerHour <0) {
+    //         return -1;
+    //     }
+
+    //     return Math.round(kilometersPerHour/1.609);
+
+    // }
+
+    // public static void printConversion (double kilometersPerHour) {
+
+    //     if (kilometersPerHour <0) {
+    //         System.out.println("Invalid Value");
+    //     } else {
+    //         long mPH = toMilesPerHour(kilometersPerHour);
+    //         System.out.println(kilometersPerHour + " km/h = " + mPH + " mi/h");
+    //     }
+
+    // }
+    // write code here
+    //I actually had the problem correct i just did not get it syntactically correct
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
             return -1;
-        }
-
-        return Math.round(kilometersPerHour/1.609);
-
+        } 
+            return Math.round(kilometersPerHour / 1.609);
+        
     }
-
-    public static void printConversion (double kilometersPerHour) {
-
-        if (kilometersPerHour <0) {
+    
+    public static void PrintConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
             System.out.println("Invalid Value");
-        } else {
-            long mPH = toMilesPerHour(kilometersPerHour);
-            System.out.println(kilometersPerHour + " km/h = " + mPH + " mi/h");
-        }
-
+            return;
+        } 
+        long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
     }
-
+    
 }
     
 
