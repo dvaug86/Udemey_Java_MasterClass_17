57f3a7b6c7c840d3499fbe9045d475f3353956da
David Vaughan <dvaug86@gmail.com>
2024-06-24 03:49:12 -0400

    my logic was sound, need to work on syntax

--- Section5/src/CodingExercise4_BarkingDog.java
+++ Section5/src/CodingExercise4_BarkingDog.java
@@ -3,21 +3,41 @@ public class CodingExercise4_BarkingDog {
         shouldWakeUp(true, 1);
         shouldWakeUp(false, 2);
         shouldWakeUp(true, 8);
-        shouldWakeUp(true, -1);
+       shouldWakeUp(true, -1); 
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        
+        if ((hourOfDay < 0) || (hourOfDay > 23)) {
+            return false;
+        }  if (barking = true && ((hourOfDay < 0) && (hourOfDay < 23))) { //ithink this is where  I erred. I wasn't wrong on logic the whole time just need to realize what is 0 and 1
+            return false;
+        }
+        return true;
         
     }
-
-    public static void shouldWakeUp(boolean barking, int hourOfDay) {
-        boolean wakeUp = true;
-        if ((hourOfDay < 0) || (hourOfDay > 23)) {
-            wakeUp = false;
-        } else if (barking = true && ((hourOfDay >= 8) && (hourOfDay <= 22))) {
-            wakeUp = false;
-        }
-        System.out.println(wakeUp);
-    }
-
+    // final test i think
+    // if ((hourOfDay < 0) || (hourOfDay > 23)) {
+    //     return false;
+    // }else if(barking = false){
+    //     return false;
+    
+    // } else if (barking = true && ((hourOfDay >= 8) && (hourOfDay <= 22))) {
+    //     return false;
+    // }
 }
+
+//close
+//   if ((hourOfDay < 0) || (hourOfDay > 23)) {
+//     return false;
+// } else if (barking = true && ((hourOfDay >= 8) && (hourOfDay <= 22))) {
+//     return false;
+// }
+// return true;
+
+
+
+
 
 // failed attempt 1
 // public static void shouldWakeUp(boolean barking, int hourOfDay){
