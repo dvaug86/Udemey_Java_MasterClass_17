bcc27ff5e2c0200c88b45aa8eeae7439b0ba9991
David Vaughan <dvaug86@gmail.com>
2024-06-28 17:35:01 -0400

    start and finish coding exercise 7

---
+++ Section5/src/CodingExercise7_EqualSumChecker.java
@@ -0,0 +1,8 @@
+public class CodingExercise7_EqualSumChecker {
+ public static void main(String[] args) {
+    
+ }   
+ public static boolean hasEqualSum(int num1, int num2, int total){
+    return((num1 + num2) == total);
+ }
+}
