1a73150c1c586f868858e0590db6f951598be1f4
David Vaughan <dvaug86@gmail.com>
2024-06-30 00:33:51 -0400

    start of challange

--- Section5/src/sec53_SecondsAndMinutesChallange.java
+++ Section5/src/sec53_SecondsAndMinutesChallange.java
@@ -1,3 +1,15 @@
+/*
+ * create a method of time from seconds into hours
+ * display remaining time of minutes and seoncds in a string
+ *  -create 2 methods with same name: getDurationString
+ *      -first method has  one parameter of type int named seconds  
+ *      -second method has 2 paramters named minutes and seonds both ints
+ *  -both methods return a string in the format shown 'XXh YYm ZZs'
+ * 
+ * this is here XX represents the number of hours, YY the number of minutes, and ZZ the number of seconds
+ * -the first method should in turn call the second method to return its results
+ */
+
 public class sec53_SecondsAndMinutesChallange {
     public static void main(String[] args) {
         
