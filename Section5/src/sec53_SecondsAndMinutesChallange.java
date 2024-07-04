3becb87d68b10684d35a0976b48f49fcb83b8881
David Vaughan <dvaug86@gmail.com>
2024-07-04 11:33:30 -0400

    bonus portion completed

--- Section5/src/sec53_SecondsAndMinutesChallange.java
+++ Section5/src/sec53_SecondsAndMinutesChallange.java
@@ -8,10 +8,19 @@
  * 
  * this is here XX represents the number of hours, YY the number of minutes, and ZZ the number of seconds
  * -the first method should in turn call the second method to return its results
+ * 
+ * Bonus: add validation. 
+ *  -first method: seconds method should be >= 0
+ *  - second mehtod minutes should be >=0 and <=59 and same for seconds
+ * -if it is invalid a message should be displayed
  */
 
 public class sec53_SecondsAndMinutesChallange {
     public static void main(String[] args) {
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 145));
+        System.out.println(getDurationString(65, 45));
         System.out.println(getDurationString(3945));
         System.out.println(getDurationString(65, 45));
 
@@ -20,6 +29,9 @@ public class sec53_SecondsAndMinutesChallange {
     public static String getDurationString(int seconds) {
 
         // 2 step approach
+        if(seconds < 0){ 
+            return "Invalid data for seconds (" + seconds + "), must be a positive integer value";
+        }
         int minutes = seconds / 60;
         // int hours = minutes / 60;
         // System.out.println("hours= " + hours);
@@ -38,10 +50,18 @@ public class sec53_SecondsAndMinutesChallange {
         // return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
 
         //to eliminate a lot of this code which can be considered redundant we will do the overload method.
-        return getDurationString(minutes, seconds);
+        return getDurationString(minutes, seconds % 60);
     }
 
     public static String getDurationString(int minutes, int seconds) {
+    
+        if(minutes < 0){ 
+            return "Invalid data for minutes (" + minutes + "), must be a positive integer value";
+        }
+        if  (seconds < 0 || seconds >59){
+return "invalid data for seconds";
+        }
+
         int hours = minutes / 60;
         int remainingMinutes = minutes % 60;
         int remainingSeconds = seconds % 60;
