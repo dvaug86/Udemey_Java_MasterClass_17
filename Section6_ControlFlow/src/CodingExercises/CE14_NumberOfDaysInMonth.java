716dd348562700121ab07208d4c8df3bfb1fd8ff
David Vaughan <dvaug86@gmail.com>
2024-07-05 12:33:45 -0400

    coding exercise 14 failed first atttempt

--- Section6_ControlFlow/src/CodingExercises/CE14_NumberOfDaysInMonth.java
+++ Section6_ControlFlow/src/CodingExercises/CE14_NumberOfDaysInMonth.java
@@ -23,4 +23,21 @@ public class CE14_NumberOfDaysInMonth {
             return false;
         }
     }
+
+    public static int getDaysinMonth(int month, int year){
+        if(((month <1) || (month >12) || ((year < 1) || (year > 9999)))){
+            return -1;
+        }
+        int days = switch(month){
+            case 1, 3, 5, 7, 8 , 10, 12 -> 31;
+            case 2 ->{
+                if(isLeapYear(year)==true){
+                    yield 29;
+                }
+                yield 28;
+            }
+            default -> 30;
+        };
+       return  days;
+    }
 }
