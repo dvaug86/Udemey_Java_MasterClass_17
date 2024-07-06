11817f3f3c2115c02dab5c362397ff9996c16c66
David Vaughan <dvaug86@gmail.com>
2024-07-06 19:28:32 -0400

    basic for statement

--- Section5_Expressions_STatements_andMore/src/sec50_TheForStatement/Main.java
+++ Section5_Expressions_STatements_andMore/src/sec50_TheForStatement/Main.java
@@ -2,6 +2,8 @@ package sec50_TheForStatement;
 
 public class Main {
     public static void main(String[] args) {
-        
+        for(int counter = 1; counter <= 5; counter++){
+            System.out.println(counter);
+        }
     }
 }
