e282fca0db9b0baf4480de0d82d11d94d78dca3b
David Vaughan <dvaug86@gmail.com>
2024-07-07 07:52:57 -0400

    as usual i overthought things

--- Section6_ControlFlow/src/sec62_Sum3and5Challange/Main.java
+++ Section6_ControlFlow/src/sec62_Sum3and5Challange/Main.java
@@ -6,23 +6,37 @@
  * After breaking out the loop print the sum of the numbers that met the conditions above
  */
 
-
-
 package sec62_Sum3and5Challange;
 
 public class Main {
     public static void main(String[] args) {
-        int count = 0;
-        for(int i = 1; i<=1000 && count <5; i++){
-            if(i %3 ==0){
-                if( i % 5 == 0){
-                    System.out.println(i + "is divisible by 3 and 5");
-                }
-                i+=i;
+
+        // my failed attempt. Again I think I'm on the right path just not sequencing it
+        // right
+        // int count = 0;
+        // for (int i = 1; i <= 1000 && count < 5; i++) {
+        // if (i % 3 == 0) {
+        // if (i % 5 == 0) {
+        // System.out.println(i + "is divisible by 3 and 5");
+        // }
+        // i += i;
+        // }
+        // count++;
+        // }
+
+        int countOfMatches = 0;
+        int sumOfMatches = 0;
+
+        for (int loopNumber = 1; loopNumber <= 1000; loopNumber++) {
+            if ((loopNumber % 3 == 0) && (loopNumber % 5 == 0)) {
+                countOfMatches++;
+                sumOfMatches += loopNumber;
+                System.out.println("Found a match= " + loopNumber);
             }
-            count++;
-            System.out.println("the sum of numbers is" +i);
+            if (countOfMatches == 5) {
+                break;
+            }
         }
-        
+        System.out.println("Sum = " + sumOfMatches);
     }
 }
