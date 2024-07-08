d6137a5073618891637337265079fee00a6b807f
David Vaughan <dvaug86@gmail.com>
2024-07-08 06:50:21 -0400

    sec 6.64 while loops

---
+++ Section6_ControlFlow/src/sec64_WhileAndDoWhileStatements/Main.java
@@ -0,0 +1,26 @@
+package sec64_WhileAndDoWhileStatements;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+        // int j = 1; // have to declare this before while loop. No space in the parenthesis for it
+//this is basic while loop which you are better off doing as a for loop like above
+        // while( j <=5){
+        // System.out.println(j);
+        // j++; //have to increment mannually
+        // }
+
+
+        // a more realistic example of when you would use a while loop
+        int j = 1;
+        while (true){
+            if(j>5){
+                break;
+            }
+            System.out.println(j);
+            j++;
+        }
+    }
+}
