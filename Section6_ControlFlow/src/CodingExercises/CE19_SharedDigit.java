fd3d31f46048d85ded50ced5807a88eec66a69ae
David Vaughan <dvaug86@gmail.com>
2024-07-09 10:03:12 -0400

    ce19 shared digit. See notes for theory. need to go to work

---
+++ Section6_ControlFlow/src/CodingExercises/CE19_SharedDigit.java
@@ -0,0 +1,35 @@
+/*
+ * 
+ * Shared Digit
+Write a method named hasSharedDigit with two parameters of type int. 
+
+Each number should be within the range of 10 (inclusive) - 99 (inclusive). If one of the numbers is not within the range, the method should return false.
+
+The method should return true if there is a digit that appears in both numbers, such as 2 in 12 and 23; otherwise, the method should return false.
+
+
+
+EXAMPLE INPUT/OUTPUT:
+
+hasSharedDigit(12, 23); → should return true since the digit 2 appears in both numbers
+
+hasSharedDigit(9, 99); → should return false since 9 is not within the range of 10-99
+
+hasSharedDigit(15, 55); → should return true since the digit 5 appears in both numbers
+ * 
+ */
+
+package CodingExercises;
+
+public class CE19_SharedDigit {
+    public static void main(String[] args) {
+        
+    }
+    public static boolean(int num1, int num2){
+        if(((num1 < 10) || (num1 >99)) || ((num2 < 10) || (num2 >99)) ){
+            return false;
+            //need to set up a while loop with in a while loop: while num 1 is running through each digit
+            //a second while loop takes each digit in num2 and compares to num 1
+        }
+    }
+}
