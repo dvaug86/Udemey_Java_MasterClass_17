81373ca05303b711e16837989934d80776a51465
David Vaughan <dvaug86@gmail.com>
2024-07-09 09:55:45 -0400

    ce18 even digit sum completed first try

---
+++ Section6_ControlFlow/src/CodingExercises/CE18_EvenDigitSum.java
@@ -0,0 +1,43 @@
+/*
+ * Even Digit Sum
+ * Write a method named getEvenDigitSum with one parameter of type int called number.
+ * 
+ * The method should return the sum of the even digits within the number.
+ * 
+ * If the number is negative, the method should return -1 to indicate an invalid value.
+ * 
+ * EXAMPLE INPUT/OUTPUT:
+ * 
+ * getEvenDigitSum(123456789); → should return 20 since 2 + 4 + 6 + 8 = 20
+ * 
+ * getEvenDigitSum(252); → should return 4 since 2 + 2 = 4
+ * 
+ * getEvenDigitSum(-22); → should return -1 since the number is negative
+ * 
+  */
+
+package CodingExercises;
+
+public class CE18_EvenDigitSum {
+    public static void main(String[] args) {
+       System.out.println("the sum of even numbers for 123456789 = " + getEvenDigitSum(123456789)); 
+       System.out.println("the sum of even numbers for 252 = " + getEvenDigitSum(252)); 
+       System.out.println("the sum of even numbers for -22 = " + getEvenDigitSum(-22)); 
+    }
+
+    public static int getEvenDigitSum(int number){
+        if(number < 0){
+            return -1;
+        }
+        int sum = 0;
+        while( number > 0){
+            int newNumber = number % 10;
+            if(newNumber % 2 == 0){
+                sum +=newNumber;
+            }
+            number = number/10;
+        }
+        return sum;
+    }
+
+}
