f412275f83d353eb3a0c5f26c4d4b8e06a553b67
David Vaughan <dvaug86@gmail.com>
2024-07-09 09:56:37 -0400

    ce18 with answer key

--- Section6_ControlFlow/src/CodingExercises/CE18_EvenDigitSum.java
+++ Section6_ControlFlow/src/CodingExercises/CE18_EvenDigitSum.java
@@ -41,3 +41,38 @@ public class CE18_EvenDigitSum {
     }
 
 }
+
+
+/*
+Answer key 
+
+
+ * public class EvenDigitSum {
+ 
+    public static int getEvenDigitSum(int number) {
+        
+        if (number < 0) {
+            return -1;
+        }
+ 
+        int sum = 0;
+        for (int i = number; i > 0; i /= 10) {
+            int lastDigit = i % 10;
+            if (lastDigit % 2 == 0) {
+                sum += lastDigit;
+            }
+        }
+ 
+        // == alternative using while loop ==
+        // while (number > 0) {
+        //     int lastDigit = number % 10;
+        //     if (lastDigit % 2 == 0) {
+        //         sum += lastDigit;
+        //     }
+        //     number /= 10;
+        // }
+        
+        return sum;
+    }
+}
+ */
