c3bd5a88c18f413973b9bb1bc9f1ef4b717e3038
David Vaughan <dvaug86@gmail.com>
2024-07-08 21:14:34 -0400

    this was tricky but i understand it makes sense after seeing an explination

--- Section6_ControlFlow/src/sec66_DigitSumChallengeExercise/Main.java
+++ Section6_ControlFlow/src/sec66_DigitSumChallengeExercise/Main.java
@@ -15,17 +15,23 @@ package sec66_DigitSumChallengeExercise;
 
 public class Main {
     public static void main(String[] args) {
-      System.out.println(sumDigits(125));  
+      System.out.println(sumDigits(1234));  
+      System.out.println(sumDigits(-125));  
+      System.out.println(sumDigits(4));  
+      System.out.println(sumDigits(32123));  
     }
     public static int sumDigits(int number){
         if(number <0){
             return -1;
         }
         int sum =0;
-        while(number >1){
-            int digit = number %10;
-            sum += digit;
+        while(number >9){  //we do this so that when it is more than 1 digit. if it is a single digit it skips the while loop
+             sum += number %10;
+            number = number /10; 
+            //forgot about the fact that number/10is will take off the last digit each time.
+            //  this will make it 125/10 = 12.5 rounded to 12 then 12/10 = 1.2 thus 1 .
         }
+        sum+= number; // this adds the final number to the sum if it is more than 1 digit. or it just takes the final digit.
         return sum;
     }
 }
