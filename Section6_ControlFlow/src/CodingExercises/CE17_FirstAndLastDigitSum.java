6e1b0867fc85a3ef20161dea63a138a6b574a65e
David Vaughan <dvaug86@gmail.com>
2024-07-09 08:16:21 -0400

    ce17 with answer key

--- Section6_ControlFlow/src/CodingExercises/CE17_FirstAndLastDigitSum.java
+++ Section6_ControlFlow/src/CodingExercises/CE17_FirstAndLastDigitSum.java
@@ -29,3 +29,24 @@ return  firstDigit + lastDigit;
         
     }
 }
+
+/*
+ * answer key
+ * public class FirstLastDigitSum {
+ 
+    public static int sumFirstAndLastDigit(int number) {
+        
+        if (number < 0) {
+            return -1;
+        }
+ 
+        int lastDigit = number % 10;  // use % 10 to get last digit in a number.
+        while (number >= 10) {
+            number /= 10;  // divide with 10 until there is 1 digit left (< 10) to get first digit.  This is also equivalent to number = number / 10.
+        }
+ 
+        int firstDigit = number; // this is not necessary, but this makes it clear that number is now the first digit.
+        return firstDigit + lastDigit;
+    }
+}
+ */
