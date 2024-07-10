70613ce06df96e8c01abd8b4f5c3c01ad6de9354
David Vaughan <dvaug86@gmail.com>
2024-07-10 07:07:14 -0400

    ce22 complete with answer key

---
+++ Section6_ControlFlow/src/CodingExercises/CE22_GreatestCommonDivisor.java
@@ -0,0 +1,72 @@
+/*
+ * Greatest Common Divisor
+Write a method named getGreatestCommonDivisor with two parameters of type int named first and second. 
+
+If one of the parameters is < 10, the method should return -1 to indicate an invalid value.
+
+The method should return the greatest common divisor of the two numbers (int).
+
+The greatest common divisor is the largest positive integer that can fully divide each of the integers (i.e. without leaving a remainder).
+
+
+
+For example 12 and 30:
+
+12 can be divided by 1, 2, 3, 4, 6, 12
+
+30 can be divided by 1, 2, 3, 5, 6, 10, 15, 30
+
+The greatest common divisor is 6 since both 12 and 30 can be divided by 6, and there is no resulting remainder.
+ */
+
+package CodingExercises;
+
+public class CE22_GreatestCommonDivisor {
+    public static void main(String[] args) {
+        getGreatestCommonDivisor(25, 15);
+    }
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if ((first < 10) || (second < 10)) {
+            return -1;
+        }
+        int smallest = 1;
+        int gcd = 1;
+        if (first < second) {
+            smallest = first;
+        }
+        smallest = second;
+
+        for (int i = 1; i <= smallest; i++) {
+            if (first % i == 0 && second % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
+
+    }
+}
+
+/*
+* I need to get better about recognizing when i can use turnery
+ * Answer Key
+ * public class GreatestCommonDivisor {
+ 
+    public static int getGreatestCommonDivisor(int first, int second) {
+        
+        if (first < 10 || second < 10) {
+            return -1;
+        }
+ 
+        int min = first < second ? first : second;
+        int gcd = 1;
+        for (int j = 1; j <= min; j++) {
+            if (first % j == 0 && second % j == 0) {
+                gcd = j;
+            }
+        }
+        return gcd;
+    }
+}
+
+ */
