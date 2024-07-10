5d1ad905470ae3a51a72d51488d9bdc56a0d2827
David Vaughan <dvaug86@gmail.com>
2024-07-10 06:25:26 -0400

    ce21 completed

--- Section6_ControlFlow/src/CodingExercises/CE21_AllFactors.java
+++ Section6_ControlFlow/src/CodingExercises/CE21_AllFactors.java
@@ -2,13 +2,17 @@ package CodingExercises;
 
 public class CE21_AllFactors {
     public static void main(String[] args) {
-        
+printFactors(10);
     }
-    
-    
-    public static int printFactors(int number){
-        if(number < 1){
-            return -1;
+
+    public static void printFactors(int number) {
+        if(number < 1) {
+            System.out.println("Invalid Value");
         }
+       for(int i = 1; i <= number; i++){
+        if(number % i ==0){
+            System.out.println(i);
+        }
+       }
     }
 }
