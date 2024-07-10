974e7f2737b0544104bb1a5afb1c16f96ab65f70
David Vaughan <dvaug86@gmail.com>
2024-07-09 20:25:00 -0400

    ce19 my code wasn't what was being looked for but still works in a more advanced capacity

--- Section6_ControlFlow/src/CodingExercises/CE19_SharedDigit.java
+++ Section6_ControlFlow/src/CodingExercises/CE19_SharedDigit.java
@@ -27,6 +27,8 @@ public class CE19_SharedDigit {
         System.out.println(" the numbers 12, and 23 is: " + hasSharedDigit(12, 23));
         System.out.println(" the numbers 9, and 99 is: " + hasSharedDigit(9, 299));
         System.out.println(" the numbers 15, and 55 is: " + hasSharedDigit(15, 55));
+        System.out.println(" the numbers 12, and 13 is: " + hasSharedDigit(12, 23));
+        System.out.println(" the numbers 999, and 891 is: " + hasSharedDigit(999, 891));
     }
 
     // need to set up a while loop with in a while loop: while num 1 is running
@@ -34,7 +36,7 @@ public class CE19_SharedDigit {
     // a second while loop takes each digit in num2 and compares to num 1
 
     public static boolean hasSharedDigit(int num1, int num2) {
-        if (((num1 < 10) || (num1 > 99)) || ((num2 < 10) || (num2 > 99))) {
+        if (((num1 < 10) || (num1 > 9999)) || ((num2 < 10) || (num2 > 9999))) {
             return false;
         }
         while (num1 > 0) {
@@ -48,6 +50,42 @@ public class CE19_SharedDigit {
             }
             num1 = num1 / 10;
         }
+        if(num2 == num1){ //this allows me to compare the last 2 numbers if the number is larger than 2 digits;
+            return true;
+        }
         return false;
     }
 }
+
+/*
+ * I can't see why my code here on VScode worked perfectly where as on Udemys enviroment it is coming up as
+ * false when comparing 12 and 13.
+ * One thing is for certain is that I was way too focused on the while loops and didn't think about the simplicity
+ * of the answer they provided as seen below. it was only going to be 2 digits so finding left and right digits is simple.
+ *My code however will allow numbers larger than 2 digits to be compared.
+ * 
+ * Below is the answer key
+ * public class SharedDigit {
+ 
+    public static boolean hasSharedDigit(int first, int second) {
+        
+        boolean firstValid = first > 9 && first < 100;
+        boolean secondValid = second > 9 && second < 100;
+ 
+        if (!firstValid || !secondValid) {
+            return false;
+        }
+ 
+        int firstLeftDigit = first / 10;
+        int firstRightDigit = first % 10;
+        int secondLeftDigit = second / 10;
+        int secondRightDigit = second % 10;
+ 
+        boolean firstShared = firstLeftDigit == secondLeftDigit || firstLeftDigit == secondRightDigit;
+        boolean secondShared = firstRightDigit == secondLeftDigit || firstRightDigit == secondRightDigit;
+ 
+        return firstShared || secondShared;
+    }
+}
+
+ */
