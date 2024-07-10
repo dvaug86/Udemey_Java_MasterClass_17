f7e118422f664c983834eced98ded1c96ed2bf55
David Vaughan <dvaug86@gmail.com>
2024-07-10 06:01:48 -0400

    ce21 All factors

---
+++ Section6_ControlFlow/src/CodingExercises/CE21_AllFactors.java
@@ -0,0 +1,14 @@
+package CodingExercises;
+
+public class CE21_AllFactors {
+    public static void main(String[] args) {
+        
+    }
+    
+    
+    public static int printFactors(int number){
+        if(number < 1){
+            return -1;
+        }
+    }
+}
