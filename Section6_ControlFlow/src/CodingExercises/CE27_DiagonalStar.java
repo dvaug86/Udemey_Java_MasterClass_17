fb7d8d1fb4c2ff9707087180f7c5fc8137d90af4
David Vaughan <dvaug86@gmail.com>
2024-07-12 14:02:20 -0400

    so close yet so far. answer key with comments

--- Section6_ControlFlow/src/CodingExercises/CE27_DiagonalStar.java
+++ Section6_ControlFlow/src/CodingExercises/CE27_DiagonalStar.java
@@ -74,7 +74,9 @@ public class CE27_DiagonalStar {
     public static void printSquareStar(int number) {
         if (number < 5) {
             System.out.println("Invalid Value");
+            return; //I need this to break out of it otherwise it would keep going
         }
+
         // row count
         for (int i = 1; i <= number; i++) {
             if (i == 1 || i == number) {
@@ -105,3 +107,46 @@ public class CE27_DiagonalStar {
         }
     }
 }
+
+
+//I am proud of my progress with this. However I made it way too complicated as usual.  
+//I should have used column and row as my variables instead of i and j. 
+// I think that would have made it more readable. 
+// Im not sure how much better it would have been if I had used the boolean part though instead of if-else statements.
+
+//below is the answer key:
+
+//public class DiagonalStar {
+//     // write code here
+//     private static final String STAR = "*";
+//     private static final String SPACE = " ";
+ 
+//     public static void printSquareStar(int number) {
+        
+//         if (number < 5) {
+//             System.out.println("Invalid Value");
+//             return;
+//         }
+ 
+//         // Iterate over each row
+//         for (int row = 1; row <= number; row++) {
+//             // Iterate over each column
+//             for (int column = 1; column <= number; column++) {
+//                 // if either row or column equals 1, assign true to isFirstRowOrColumn.
+//                 boolean isFirstRowOrColumn = row == 1 || column == 1;
+//                 // if either row or column equals number, assign true to isLastRowOrColumn.
+//                 boolean isLastRowOrColumn = row == number || column == number;
+//                 // if either row equals column, or column equals number minus row plus 1, assign true to isFirstRowOrColumn.
+//                 boolean isDiagonal = (row == column) || (column == (number - row + 1));
+ 
+//                 if (isFirstRowOrColumn || isLastRowOrColumn || isDiagonal) {
+//                     System.out.print(STAR);
+//                 } else {
+//                     System.out.print(SPACE);
+//                 }
+//             }
+//             // Move to the next line
+//             System.out.println();
+//         }
+//     }
+// }
