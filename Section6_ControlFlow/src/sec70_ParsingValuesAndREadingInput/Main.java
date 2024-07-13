de3d1c7cc23b8894ac0824ba5b2756b15f750986
David Vaughan <dvaug86@gmail.com>
2024-07-13 08:56:51 -0400

    string + int

--- Section6_ControlFlow/src/sec70_ParsingValuesAndREadingInput/Main.java
+++ Section6_ControlFlow/src/sec70_ParsingValuesAndREadingInput/Main.java
@@ -3,5 +3,20 @@ package sec70_ParsingValuesAndREadingInput;
 public class Main {
     public static void main(String[] args) {
         
-    }
+        // String currentYear = "2024";
+        // String usersDateOfBirth = "1999";
+
+        // System.out.println("Age =" + (currentYear + usersDateOfBirth)); 
+        //tried  (currentYear - usersDateOfBirth) but that shows error
+        // error warning does show (currentYear + usersDateOfBirth) will work;
+        // as we see it concatenates the 2 strings so it shows 20241999
+        
+        //now we try with currentYear being the literal numeric value.
+        int currentYear = 2024;
+        String usersDateOfBirth = "1999";
+
+        System.out.println("Age =" + (currentYear + usersDateOfBirth)); 
+        //we get the same result.
+
+    }   
 }
