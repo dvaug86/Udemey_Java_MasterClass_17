8f4167a3d9e356c3052e26ffc98b625c2b201a74
David Vaughan <dvaug86@gmail.com>
2024-07-15 07:02:38 -0400

    stipulations of challange

--- Section6_ControlFlow/src/sec73_ReadingUserInputChallange/Main.java
+++ Section6_ControlFlow/src/sec73_ReadingUserInputChallange/Main.java
@@ -1,3 +1,21 @@
+/*
+ * In this challange, you'll read 5 valid numbers from the console entered by the user
+ *  and print the sum of those 5 numbers.
+ * 
+ * You need to check that the numbers are valid integers
+ *      -> If they aren't print a message that reads "Invalid number" to the console
+ *           but continue looping until  you do have 5 valid numbers.
+ * 
+ * Before the user enters each number, prompt them with the message, " enter number #x:"
+ * where x represents the count 1,2,3 etc.
+ * 
+ * As an example the first message would look like " Enter number #1: " 
+ *      the next "Enter number #2: " and so on.
+ * 
+ * use a while or do while plus scanner and next line method.
+ * 
+ */
+
 package sec73_ReadingUserInputChallange;
 
 public class Main {
