77df634acde7ec020931f787d3814f015ab64776
David Vaughan <dvaug86@gmail.com>
2024-07-16 06:37:36 -0400

    i was on the right path I just didn't think about the loop to get it started.

--- Section6_ControlFlow/src/sec74_MinAndMaxChallenge/Main.java
+++ Section6_ControlFlow/src/sec74_MinAndMaxChallenge/Main.java
@@ -19,29 +19,60 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int min;
-        int max;
-        System.out.println("In order to exit enter a character or 999");
-        System.out.println("enter a number!");
-        String nextNumber = scanner.nextLine();
 
-        int number;
-        if (  number != 999) {
+        // my try:
+
+        // int min;
+        // int max;
+        // System.out.println("In order to exit enter a character or 999");
+        // System.out.println("enter a number!");
+        // String nextNumber = scanner.nextLine();
+
+        // int number;
+        // if ( number != 999) {
+        // try {
+        // number = Integer.parseInt(nextNumber);
+        // min = number;
+        // max = number;
+        // if(number < min){
+        // min = number;
+        // System.out.println("the new min is: " + min);
+        // }
+        // if(number > max){
+        // max= number;
+        // System.out.println("the new max is: " + max);
+        // }
+        // } catch (NumberFormatException nfe) {
+        // System.out.println("Goodbye");
+        // }
+        // }
+
+        // answer key:
+        double max = 0;
+        double min = 0;
+        int loopCount = 0; //only really necessary for the initiaztion.
+
+        while (true) {
+            System.out.println("In order to exit enter a character or number");
+            String nextEntry = scanner.nextLine();
             try {
-                number = Integer.parseInt(nextNumber);
-               min = number;
-               max = number;
-               if(number < min){
-                min = number;
-                System.out.println("the new min is: " + min);
-               }
-               if(number > max){
-                max= number;
-                System.out.println("the new max is: " + max);
-               }
+                double validNum = Double.parseDouble(nextEntry);
+                if (loopCount == 0 || validNum < min) { // checking loopcount == 0 is where i was lost. this allows us
+                                                        // to set the first number enterd as min or max.
+                    min = validNum;
+                }
+                if (loopCount == 0 || validNum > max) {
+                    max = validNum;
+                }
+                loopCount++;
             } catch (NumberFormatException nfe) {
-                System.out.println("Goodbye");
+                break; // did not do this.
             }
+        }
+        if (loopCount > 0) {
+            System.out.println("min = " +min+ ", max = " + max);
+        } else{
+            System.out.println("no valid data entered");
         }
     }
 }
