ce42c586ead6dec4b08bad23e1d2bbb08b994d90
David Vaughan <dvaug86@gmail.com>
2024-07-16 06:02:59 -0400

    sec 74 min and max challenge

---
+++ Section6_ControlFlow/src/sec74_MinAndMaxChallenge/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Use an endless loop which:
+ *      -> prompts the user to enter a number or any character to quit.
+ *      -> Validates if the user-entered data really is a number.
+ *              You can choose either an integer or double validation method.
+ *      -> If the user-entered data is not a number, quit the loop.
+ *      -> Keep track of the minimum number entered.
+ *      -> Keep track of the maximum number entered.
+ *      -> if the user has previously entered a set of numbers  (or even just one),
+ *              display the min and max number that the user entered.
+ *      
+ */
+
+
+
+package sec74_MinAndMaxChallenge;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
