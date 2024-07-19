9317e152e1ee107f3e19ff09c2046b1411a5fc61
David Vaughan <dvaug86@gmail.com>
2024-07-19 15:34:45 -0400

    sec 79 classes challange

--- Section7_OOP_Part1_Inheritance/src/Sections/sec79_ClassesChallangeExercise/Account.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec79_ClassesChallangeExercise/Account.java
@@ -2,4 +2,64 @@ package Sections.sec79_ClassesChallangeExercise;
 
 public class Account {
 
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
+
+    public void depositFunds(double depositAmount) {
+        balance += depositAmount;
+        System.out.println("Deposit of $" + depositAmount + " made. New balance is $" + this.balance);
+    }
+
+    public void withdrawFunds(double withdrawlAmount) {
+        if (balance - withdrawlAmount < 0) {
+            System.out.println("Insufficient Funds! You only have $" + balance + " in your account.");
+        } else {
+            balance -= withdrawlAmount;
+            System.out
+                    .println("Withdrawl of $" + withdrawlAmount + " processed. Remaining balance is $" + this.balance);
+        }
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
 }
--- Section7_OOP_Part1_Inheritance/src/Sections/sec79_ClassesChallangeExercise/Main.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec79_ClassesChallangeExercise/Main.java
@@ -22,6 +22,15 @@ package Sections.sec79_ClassesChallangeExercise;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Account bobsAccount = new Account();
+        bobsAccount.setNumber("12345");
+        bobsAccount.setBalance(1000.00);
+        bobsAccount.setCustomerName("Bob Brown");
+bobsAccount.setCustomerEmail("bobsEmail@email.com");
+bobsAccount.setCustomerPhone("123-456-7890");
+
+        bobsAccount.withdrawFunds(100.0);
+        bobsAccount.depositFunds(250);
+        bobsAccount.withdrawFunds(50);
     }
 }
