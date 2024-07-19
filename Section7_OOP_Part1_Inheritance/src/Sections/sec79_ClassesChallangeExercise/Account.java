c012b580a949b9fa69fc9a367a8e06d40555e9da
David Vaughan <dvaug86@gmail.com>
2024-07-19 15:05:56 -0400

    start of classes challange

---
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec79_ClassesChallangeExercise/Account.java
@@ -0,0 +1,5 @@
+package Sections.sec79_ClassesChallangeExercise;
+
+public class Account {
+
+}
---
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec79_ClassesChallangeExercise/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Create a new class for a bank account
+ * 
+ * Create fields for account characteristics like:
+ *      -> account number
+ *      -> account balance
+ *      -> customer name
+ *      -> email
+ *      -> phone number
+ *
+ * Create getters and setters for each field
+ * 
+ * create 2 additional methods
+ *      -1. depositing funds into the account
+ *      -2. withdraw funds for the account
+ *              -customer should not be allowed to withdraw funds if that takes their balance negative
+ *      
+ */
+
+
+package Sections.sec79_ClassesChallangeExercise;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
