8f4ddf4c0337ffaf3e1f22087bd40f287597011e
David Vaughan <dvaug86@gmail.com>
2024-07-20 11:59:49 -0400

    start of sec80 constructors part 1

---
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec80_ConstructorsPt1/Account.java
@@ -0,0 +1,10 @@
+package Sections.sec80_ConstructorsPt1;
+
+public class Account {    //This is the class declaration
+
+        public Account(){ // this is the constructor declaration
+
+            //Constructor code is code to be executed as the object is createde
+
+        }
+}
