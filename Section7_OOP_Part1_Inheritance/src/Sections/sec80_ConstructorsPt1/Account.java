aad0c58b0f8805661ba9f0fad4f9c4ec2ca9c251
David Vaughan <dvaug86@gmail.com>
2024-07-21 19:42:08 -0400

    end of sec80 constructor part 1

--- Section7_OOP_Part1_Inheritance/src/Sections/sec80_ConstructorsPt1/Account.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec80_ConstructorsPt1/Account.java
@@ -2,10 +2,29 @@ package Sections.sec80_ConstructorsPt1;
 
 public class Account { // This is the class declaration
     private String number;
+
     private double balance;
+
     private String customerName;
+
     private String customerEmail;
+
     private String customerPhone;
+
+//we are creating our own constructor here
+public Account() {
+    System.out.println("Empty constructor called");
+}
+
+//we are creating a constructor here with arguments:
+public Account(String number, double balance, String customerName, String email, String phone ){
+    System.out.println(" Account constructor with parameters called");
+    this.number = number;
+    this.balance = balance;
+    this.customerName = customerName;
+    customerEmail = email;
+    customerPhone = phone;
+}
 
     public void depositFunds(double depositAmount) {
         balance += depositAmount;
@@ -62,9 +81,9 @@ public class Account { // This is the class declaration
         this.customerPhone = customerPhone;
     }
 
-    public Account() { // this is the constructor declaration
+    // public Account() { // this is the constructor declaration
 
         // Constructor code is code to be executed as the object is createde
 
-    }
+    // }
 }
--- Section7_OOP_Part1_Inheritance/src/Sections/sec80_ConstructorsPt1/Main.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec80_ConstructorsPt1/Main.java
@@ -3,12 +3,19 @@ package Sections.sec80_ConstructorsPt1;
 public class Main {
 
     public static void main(String[] args) {
-        Account bobsAccount = new Account();
-        bobsAccount.setNumber("12345");
-        bobsAccount.setBalance(1000.00);
-        bobsAccount.setCustomerName("Bob Brown");
-        bobsAccount.setCustomerEmail("bobsEmail@email.com");
-        bobsAccount.setCustomerPhone("(123)-456-7890");
+
+        // we can knockout everything we did with the setter in one bit of code below.
+        Account bobsAccount = new Account("12345", 1000.00, "Bob Brown",
+                "bobsEmail@email.com", "(123)-456-7890");
+
+                System.out.println(bobsAccount.getNumber());
+                System.out.println(bobsAccount.getBalance());
+        // Account bobsAccount = new Account();
+        // bobsAccount.setNumber("12345");
+        // bobsAccount.setBalance(1000.00);
+        // bobsAccount.setCustomerName("Bob Brown");
+        // bobsAccount.setCustomerEmail("bobsEmail@email.com");
+        // bobsAccount.setCustomerPhone("(123)-456-7890");
 
         bobsAccount.withdrawFunds(100.0);
         bobsAccount.depositFunds(250);
