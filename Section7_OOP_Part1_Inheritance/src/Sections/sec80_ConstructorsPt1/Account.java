dfc5ff96b91ad53376db37a3871c9bf701a30691
David Vaughan <dvaug86@gmail.com>
2024-07-21 08:14:39 -0400

    udating sec80

--- Section7_OOP_Part1_Inheritance/src/Sections/sec80_ConstructorsPt1/Account.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec80_ConstructorsPt1/Account.java
@@ -1,10 +1,70 @@
 package Sections.sec80_ConstructorsPt1;
 
-public class Account {    //This is the class declaration
+public class Account { // This is the class declaration
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
 
-        public Account(){ // this is the constructor declaration
+    public void depositFunds(double depositAmount) {
+        balance += depositAmount;
+        System.out.println("Deposit of $" + depositAmount + " made. New balance is $" + this.balance);
+    }
 
-            //Constructor code is code to be executed as the object is createde
-
+    public void withdrawFunds(double withdrawlAmount) {
+        if (balance - withdrawlAmount < 0) {
+            System.out.println("Insufficient Funds! You only have $" + balance + " in your account.");
+        } else {
+            balance -= withdrawlAmount;
+            System.out
+                    .println("Withdrawl of $" + withdrawlAmount + " processed. Remaining balance is $" + this.balance);
         }
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public Account() { // this is the constructor declaration
+
+        // Constructor code is code to be executed as the object is createde
+
+    }
 }
--- Section7_OOP_Part1_Inheritance/src/Sections/sec80_ConstructorsPt1/Main.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec80_ConstructorsPt1/Main.java
@@ -1,7 +1,17 @@
 package Sections.sec80_ConstructorsPt1;
 
 public class Main {
+
     public static void main(String[] args) {
-        
+        Account bobsAccount = new Account();
+        bobsAccount.setNumber("12345");
+        bobsAccount.setBalance(1000.00);
+        bobsAccount.setCustomerName("Bob Brown");
+        bobsAccount.setCustomerEmail("bobsEmail@email.com");
+        bobsAccount.setCustomerPhone("(123)-456-7890");
+
+        bobsAccount.withdrawFunds(100.0);
+        bobsAccount.depositFunds(250);
+        bobsAccount.withdrawFunds(50);
     }
 }
