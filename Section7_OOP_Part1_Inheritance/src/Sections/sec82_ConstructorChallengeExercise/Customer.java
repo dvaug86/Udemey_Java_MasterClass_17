f8d75121b235ed9446597337454de2c928c9c102
David Vaughan <dvaug86@gmail.com>
2024-07-23 08:12:51 -0400

    guidelines of challange and start of customer class

---
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec82_ConstructorChallengeExercise/Customer.java
@@ -0,0 +1,8 @@
+package Sections.sec82_ConstructorChallengeExercise;
+
+public class Customer {
+    private String name;
+    private int creditLimit;
+    private String emailAddress;
+
+}
---
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec82_ConstructorChallengeExercise/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Create a new classs, called Customer, with three fields
+ *      1. name
+ *      2. credit limit
+ *      3. email address
+ * 
+ * Create the getter methods only for each field. you don't need to create the setters.
+ * 
+ * Create 3 constructors for this class:
+ *      -> 1. create a constructor for all three fields which should assign arguments directly to the instance fields
+ *  
+ *      -> 2. Create a no-args constructor that calls another constructor, passing some literal values for each argument.
+ * 
+ *      -> 3. Create a constructor with just the name and email parameters, which also calls another constructor.
+ */
+
+
+
+package Sections.sec82_ConstructorChallengeExercise;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
