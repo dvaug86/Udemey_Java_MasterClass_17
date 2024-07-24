9b589412a41afd14d7b36ba1efbb5b000d454223
David Vaughan <dvaug86@gmail.com>
2024-07-24 09:28:13 -0400

    sec85 static vs instance methods

--- Section7_OOP_Part1_Inheritance/src/Sections/sec85_StaticVsInstanceMethods/Calculator.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec85_StaticVsInstanceMethods/Calculator.java
@@ -2,4 +2,5 @@ package Sections.sec85_StaticVsInstanceMethods;
 
 public class Calculator {
 
+        
 }
---
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec85_StaticVsInstanceMethods/Main.java
@@ -0,0 +1,7 @@
+package Sections.sec85_StaticVsInstanceMethods;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
