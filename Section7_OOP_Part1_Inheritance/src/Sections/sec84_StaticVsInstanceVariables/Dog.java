c55a88aa477975dd6b39b30d775b96c900202fd2
David Vaughan <dvaug86@gmail.com>
2024-07-24 08:48:43 -0400

    end of sec84 static vs instance variables

--- Section7_OOP_Part1_Inheritance/src/Sections/sec84_StaticVsInstanceVariables/Dog.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec84_StaticVsInstanceVariables/Dog.java
@@ -2,15 +2,32 @@ package Sections.sec84_StaticVsInstanceVariables;
 
 public class Dog {
 
-    static String genus = "Canis";
+    // static String genus = "Canis";
 
-    void printData(){
-        Dog d = new Dog();
-        System.out.println(d.genus); //confusing
+    // void printData(){
+    //     Dog d = new Dog();
+    //   //  System.out.println(d.genus); //confusing and vsCode has issues with it.
 
-        //It is considered best practice to  use the Class name and not a reference variable to access a static varible
-        System.out.println(Dog.genus); //clearer
-        //this makes it clear that the variable is associated iwth the class and not stored with the instance.
-    }
+    //     //It is considered best practice to  use the Class name and not a reference variable to access a static varible
+    //     System.out.println(Dog.genus); //clearer
+    //     //this makes it clear that the variable is associated iwth the class and not stored with the instance.
+    // }
+/*
+This is an example of how static variables should not be used as this causes every instance of name to change in the main class
+private static String name;
+public Dog(String name){
+    Dog.name = name;
+}
+public void printName(){
+    System.out.println("name = " + name); // using Dog.name would have made this code less confusing.
+}
+*/
+private String name; // field declared
+public Dog(String name){
+    this.name = name;
+}
 
+public void printName(){
+    System.out.println("name = " + name);
+}
 }
--- Section7_OOP_Part1_Inheritance/src/Sections/sec84_StaticVsInstanceVariables/Main.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec84_StaticVsInstanceVariables/Main.java
@@ -3,7 +3,18 @@ package Sections.sec84_StaticVsInstanceVariables;
 public class Main {
     public static void main(String[] args) {
         
-        System.out.println(Dog.genus); //no instance of dog needs to exist in order to access a static variable.
+        // System.out.println(Dog.genus); //no instance of dog needs to exist in order to access a static variable.
 
+        Dog rex = new Dog("rex"); //create instance (rex)
+        Dog fluffy = new Dog("fluffy"); //create instance (fluffy);
+
+        rex.printName();//prints fluffy
+        fluffy.printName(); //prints fluffy.
+
+        //both instances print fluffy b.c name is static in the dog class. when object dog gets a name it is the same 
+        //name through out every instance of dog. when a new dog is created with a new name it changes the name to that name
+        //for all the old dogs.
+
+        //with the new code in Dog where it is no longer a static variable. The result of the code is how we want it.
     }
 }
