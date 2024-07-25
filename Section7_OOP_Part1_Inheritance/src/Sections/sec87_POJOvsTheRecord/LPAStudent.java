5685e7bc64ced3072fff72849b19436eb83942a9
David Vaughan <dvaug86@gmail.com>
2024-07-25 09:35:17 -0400

    completion of sec 87

--- Section7_OOP_Part1_Inheritance/src/Sections/sec87_POJOvsTheRecord/LPAStudent.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec87_POJOvsTheRecord/LPAStudent.java
@@ -2,6 +2,9 @@ package Sections.sec87_POJOvsTheRecord;
 
 
 //notice the use of the word record instead of class
-public record LPAStudent() {
+//notice also the parentheses which allow us to add parameters
+
+//the parameters are called the record header
+public record LPAStudent(String id, String name, String dateOfBirth, String classList) {
 
 }
--- Section7_OOP_Part1_Inheritance/src/Sections/sec87_POJOvsTheRecord/Main.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec87_POJOvsTheRecord/Main.java
@@ -3,7 +3,10 @@ package Sections.sec87_POJOvsTheRecord;
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <=5; i++){
-            Student s = new Student("S92300" +i, 
+            //we change Student from the student class to LPAStudent of LPAStudent record
+
+            // If you look at LPAStudent and Student side by side the LPAStudent eliminates a ton of code that is in Student
+            LPAStudent s = new LPAStudent("S92300" +i,  
             switch(i){
              case 1 -> "Marry";
              case 2 -> "Carol";   
@@ -16,5 +19,18 @@ public class Main {
              "Java MasterClass");
              System.out.println(s);
         }
+        Student pojoStudent = new Student("S923006", "Ann", "05/11/1985", "Java MasterClass");
+
+        LPAStudent recordStudent = new LPAStudent("S923007", "Bill", "05/11/1985", "Java MasterClass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+       // recordStudent.setClassList(recordStudent.getClassList() + ", Java OCP Exam 829");  this won't work because records don't have setters
+
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList() );
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList() );
+
     }
 }
