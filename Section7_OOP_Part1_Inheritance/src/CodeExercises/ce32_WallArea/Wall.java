7c195a11636aef9a6328a1411f22a6cc85aae66b
David Vaughan <dvaug86@gmail.com>
2024-07-26 11:52:09 -0400

    start of ce32 area of wall

---
+++ Section7_OOP_Part1_Inheritance/src/CodeExercises/ce32_WallArea/Wall.java
@@ -0,0 +1,16 @@
+package CodeExercises.ce32_WallArea;
+
+public class Wall {
+private double width;
+private double height;
+
+public Wall() {
+}
+
+public Wall(double width, double height) {
+    this.width = width;
+    this.height = height;
+}
+
+
+}
