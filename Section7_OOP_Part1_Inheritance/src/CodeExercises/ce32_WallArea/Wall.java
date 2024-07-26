1f9af361db91787959d211c8004539f57de76272
David Vaughan <dvaug86@gmail.com>
2024-07-26 13:04:35 -0400

    my code wasn't really wrong it just clashed with udemeys main class

--- Section7_OOP_Part1_Inheritance/src/CodeExercises/ce32_WallArea/Wall.java
+++ Section7_OOP_Part1_Inheritance/src/CodeExercises/ce32_WallArea/Wall.java
@@ -9,30 +9,87 @@ public class Wall {
 
     public Wall(double width, double height) {
 
-        System.out.println("area= " + getArea());
+        // I was wrong here
+        // this wasn't supposed to be here at the time
+
+        // System.out.println("area= " + getArea());
+
+        // This is what should have been here
+        // this was my only error. My original code wasn't wrong it just wasn't viable
+        // to show the height and width for what udemey's main class was doing.
+
+        setWidth(width);
+        setHeight(height);
     }
 
-    public void setWidth(double width) {
-
-        this.width = width > 0 ? width : 0;
-
-    }
     public double getWidth() {
         return width;
     }
 
+    // terenery was from the hints: i had the if statement originally and my code
+    // was right.
 
-    public void setHeight(double height) {
-        this.height = height > 0 ? height : 0;
+    public void setWidth(double width) {
+        this.width = width > 0 ? width : 0;
+
     }
+
     public double getHeight() {
         return height;
     }
 
+    public void setHeight(double height) {
+        this.height = height > 0 ? height : 0;
+    }
 
     public double getArea() {
+        // This is fine but just doing "return width * height" would have been better
         double area = width * height;
         return area;
     }
 
 }
+
+// solution
+/*
+ * public class Wall {
+ * 
+ * private double width;
+ * private double height;
+ * 
+ * public Wall() {}
+ * 
+ * public Wall(double width, double height) {
+ * setWidth(width);
+ * setHeight(height);
+ * }
+ * 
+ * public double getWidth() {
+ * return width;
+ * }
+ * 
+ * public void setWidth(double width) {
+ * 
+ * if (width < 0) {
+ * width = 0;
+ * }
+ * this.width = width;
+ * }
+ * 
+ * public double getHeight() {
+ * return height;
+ * }
+ * 
+ * public void setHeight(double height) {
+ * 
+ * if (height < 0) {
+ * height = 0;
+ * }
+ * this.height = height;
+ * }
+ * 
+ * public double getArea() {
+ * return width * height;
+ * }
+ * }
+ */
