eb7a8f051e918cfb072bec965be6615f954a8c86
David Vaughan <dvaug86@gmail.com>
2024-07-26 11:58:27 -0400

    first attempt

--- Section7_OOP_Part1_Inheritance/src/CodeExercises/ce32_WallArea/Wall.java
+++ Section7_OOP_Part1_Inheritance/src/CodeExercises/ce32_WallArea/Wall.java
@@ -10,7 +10,33 @@ public Wall() {
 public Wall(double width, double height) {
     this.width = width;
     this.height = height;
+    if(width < 0 ){
+        this.width = 0;
+    }
+    if(height < 0){
+        this.width = 0;
+    }
+    System.out.println(getArea());
 }
 
+public double getWidth() {
+    return width;
+}
+
+public void setWidth(double width) {
+    this.width = width;
+}
+
+public double getHeight() {
+    return height;
+}
+
+public void setHeight(double height) {
+    this.height = height;
+}
+public double getArea(){
+    double area = width * height;
+    return area;
+}
 
 }
