36c55452362caa6fa397d69d3bd7367d53377d4c
David Vaughan <dvaug86@gmail.com>
2024-07-29 09:01:38 -0400

    start of ce 33

---
+++ Section7_OOP_Part1_Inheritance/src/CodeExercises/ce34_CarpetCostCalc/Floor.java
@@ -0,0 +1,14 @@
+package CodeExercises.ce34_CarpetCostCalc;
+
+public class Floor {
+private double width;
+private double length;
+
+
+public Floor(double width, double length) {
+    this.width = width;
+    this.length = length;
+}
+
+
+}
