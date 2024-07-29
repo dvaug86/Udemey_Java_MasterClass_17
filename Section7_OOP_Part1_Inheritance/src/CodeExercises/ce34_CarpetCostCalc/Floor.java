0646c8ecf7251b53f848353d2158d116806cd046
David Vaughan <dvaug86@gmail.com>
2024-07-29 09:08:12 -0400

    udemey website is down for maintenance

--- Section7_OOP_Part1_Inheritance/src/CodeExercises/ce34_CarpetCostCalc/Floor.java
+++ Section7_OOP_Part1_Inheritance/src/CodeExercises/ce34_CarpetCostCalc/Floor.java
@@ -6,9 +6,32 @@ private double length;
 
 
 public Floor(double width, double length) {
-    this.width = width;
-    this.length = length;
+    setWidth(width);
+    setLength(length);
 }
 
+
+public double getWidth() {
+    return width;
+}
+
+
+public void setWidth(double width) {
+    this.width = width > 0 ? width : 0;
+}
+
+
+public double getLength() {
+    return length;
+}
+
+
+public void setLength(double length) {
+    this.length = length > 0 ? length : 0;
+}
+
+public double getArea(){
+    return (width * length);
+}
 
 }
