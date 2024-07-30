88f782103acf8edf52331f0de8841ae9092c1dca
David Vaughan <dvaug86@gmail.com>
2024-07-30 06:43:05 -0400

    added carpet class

---
+++ Section7_OOP_Part1_Inheritance/src/CodeExercises/ce34_CarpetCostCalc/Carpet.java
@@ -0,0 +1,15 @@
+package CodeExercises.ce34_CarpetCostCalc;
+
+public class Carpet {
+private double cost;
+
+public Carpet(double cost) {
+    this.cost = cost > 0 ? cost : 0;
+}
+
+public double getCost() {
+    return cost;
+}
+
+
+}
