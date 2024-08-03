37046b0155c4a4f95b0c39325224e0a7efdbd00a
David Vaughan <dvaug86@gmail.com>
2024-08-03 09:49:01 -0400

    adding all the files

---
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec89_InheritancePt2/Dog.java
@@ -0,0 +1,12 @@
+package Sections.sec89_InheritancePt2;
+
+public class Dog extends Animal  {
+    public Dog(){
+       //This default with no set default variables
+       //super(); //this was coming up as an error because Animal didn't have a default constructor
+                   //I have since added it.
+       
+       //here i have super with new default variables
+       super("mutt", "big", 50.0); //these are the arguments from animal class
+    }
+   }
--- Section7_OOP_Part1_Inheritance/src/Sections/sec89_InheritancePt2/Main.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec89_InheritancePt2/Main.java
@@ -2,6 +2,18 @@ package Sections.sec89_InheritancePt2;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Animal animal = new Animal("Generic Animal", "huge", 400);
+        doAnimalStuff(animal, "slow");
+
+        Dog myDog = new Dog();
+        doAnimalStuff(myDog, "fast");
+
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _ _ _");
     }
 }
