64039ce6fb6999dc9227ca83632e6f209d2e2204
David Vaughan <dvaug86@gmail.com>
2024-08-04 08:53:34 -0400

    updated Dog class methods

--- Section7_OOP_Part1_Inheritance/src/Sections/sec90_InheritancePt3/Dog.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec90_InheritancePt3/Dog.java
@@ -1,44 +1,68 @@
 package Sections.sec90_InheritancePt3;
 
-public class Dog extends Animal  {
-    
+public class Dog extends Animal {
+
     private String earShape;
     private String tailShape;
-    
-    
-    public Dog(){
-       super("mutt", "big", 50.0); //these are the arguments from animal class
+
+    public Dog() {
+        super("mutt", "big", 50.0); // these are the arguments from animal class
     }
 
-    public Dog(String type, double weight){
+    public Dog(String type, double weight) {
         this(type, weight, "Perky", "Curled");
-         //this calls constructor with 4 args below. which calls the animal constructor ( super() ) as seen below.
+        // this calls constructor with 4 args below. which calls the animal constructor
+        // ( super() ) as seen below.
     }
 
-    public Dog(String type,  double weight, String earShape, String tailShape) {
-        super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), weight ); //size is derived from weight
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), weight); // size is derived from weight
         this.earShape = earShape;
         this.tailShape = tailShape;
     }
 
-
     @Override
-    //this varies from intellij. I have just modified it to be like it for now.
-    public String toString() { 
+    // this varies from intellij. I have just modified it to be like it for now.
+    public String toString() {
         return "Dog {earShape=" + earShape + '\'' +
-         ", tailShape=" + tailShape + '\'' + 
-         "} " + super.toString(); //this is calling the section in animal class's toString()
+                ", tailShape=" + tailShape + '\'' +
+                "} " + super.toString(); // this is calling the section in animal class's toString()
     }
 
+    public void makeNoise() {
 
-   public void makeNoise(){
-    
-   }
+    }
 
-@Override
-public void move(String speed) {
-    // TODO Auto-generated method stub
-    super.move(speed);
-    System.out.println("Dogs walk, run and wag their tail");
-}
+    @Override
+    public void move(String speed) {
+        // TODO Auto-generated method stub
+        super.move(speed);
+        // System.out.println("Dogs walk, run and wag their tail");
+        if (speed == "slow"){
+            walk();
+            wagTail();
+        } else{
+            run();
+            bark();
+        }
+        System.out.println();
+    }
+
+    // we are making this method private because it is being called by the move
+    // method above only. Not from animal
+    private void bark() {
+        System.out.println("Woof! ");
+    }
+
+    private void run() {
+        System.out.println("Dog Running ");
+    }
+
+    private void walk() {
+        System.out.println("Dog Walking ");
+    }
+
+    private void wagTail() {
+        System.out.println("Tail Wagging ");
+    }
 }
