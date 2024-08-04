646aa3728815e5853270fef42f0f6686f3936699
David Vaughan <dvaug86@gmail.com>
2024-08-04 07:48:27 -0400

    start of sec 90 inheritance pt 3

---
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec90_InheritancePt3/Dog.java
@@ -0,0 +1,44 @@
+package Sections.sec90_InheritancePt3;
+
+public class Dog extends Animal  {
+    
+    private String earShape;
+    private String tailShape;
+    
+    
+    public Dog(){
+       super("mutt", "big", 50.0); //these are the arguments from animal class
+    }
+
+    public Dog(String type, double weight){
+        this(type, weight, "Perky", "Curled");
+         //this calls constructor with 4 args below. which calls the animal constructor ( super() ) as seen below.
+    }
+
+    public Dog(String type,  double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), weight ); //size is derived from weight
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+
+    @Override
+    //this varies from intellij. I have just modified it to be like it for now.
+    public String toString() { 
+        return "Dog {earShape=" + earShape + '\'' +
+         ", tailShape=" + tailShape + '\'' + 
+         "} " + super.toString(); //this is calling the section in animal class's toString()
+    }
+
+
+   public void makeNoise(){
+    
+   }
+
+@Override
+public void move(String speed) {
+    // TODO Auto-generated method stub
+    super.move(speed);
+    System.out.println("Dogs walk, run and wag their tail");
+}
+}
---
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec90_InheritancePt3/Main.java
@@ -0,0 +1,23 @@
+package Sections.sec90_InheritancePt3;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Generic Animal", "huge", 400);
+        doAnimalStuff(animal, "slow");
+
+        Dog myDog = new Dog();
+        doAnimalStuff(myDog, "fast");
+
+        Dog yorkie = new Dog("Yorkie", 15);
+        doAnimalStuff(yorkie, "fast" );
+
+        Dog retriever = new Dog("Labrador Retriever", 65, "Floppy", "Swimmer" );
+        doAnimalStuff(retriever, "slow");
+    }
+    public static void doAnimalStuff(Animal animal, String speed) {
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _ _ _");
+    }
+}
