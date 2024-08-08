07958b5d7fbb358e150f2bad19c496bd8a4c4551
David Vaughan <dvaug86@gmail.com>
2024-08-08 11:16:51 -0400

    end of sec 94. I understand but as suggested I should come back and review

--- Section7_OOP_Part1_Inheritance/src/Sections/sec94_ThisVsSuper/Rectangle.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec94_ThisVsSuper/Rectangle.java
@@ -1,55 +1,59 @@
-package Sections.sec94_ThisVsSuper;
+// package Sections.sec94_ThisVsSuper;
 
-public class Rectangle {
+//for part 3 we got to shape  for the rectangle class
+// public class Rectangle  {
 
 
-    //Example of bad code
-   /*  private int x;
-    private int y;
-    private int width;
-    private int height;
-    public Rectangle() {
-        this.x = 0;
-        this.y = 0;
-        this.width = 0;
-        this.height = 0;
-    }
-    public Rectangle( int width, int height) {
-        this.x = 0;
-        this.y = 0;
-        this.width = width;
-        this.height = height;
-    }
-    public Rectangle(int x, int y, int width, int height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
-*/
-   //Good code
-   private int x;
-   private int y;
-   private int width;
-   private int height;
+
+ 
+//     //Example of bad code
+//    /*  private int x;
+//     private int y;
+//     private int width;
+//     private int height;
+//     public Rectangle() {
+//         this.x = 0;
+//         this.y = 0;
+//         this.width = 0;
+//         this.height = 0;
+//     }
+//     public Rectangle( int width, int height) {
+//         this.x = 0;
+//         this.y = 0;
+//         this.width = width;
+//         this.height = height;
+//     }
+//     public Rectangle(int x, int y, int width, int height) {
+//         this.x = x;
+//         this.y = y;
+//         this.width = width;
+//         this.height = height;
+//     }
+// */
+//    //Good code
+//    //this is following on and int x and y aren't necessary since it is extending y
+// //    private int x;
+// //    private int y;
+//    private int width;
+//    private int height;
    
-   //first constructor
-   public Rectangle(){
-    this(0, 0 ); //calls 2nd constructor
-   }
+//    //first constructor
+//    public Rectangle(){
+//     this(0, 0 ); //calls 2nd constructor
+//    }
 
-   //second constructor
-   public Rectangle(int width, int height){
-    this(0,0, width, height); //calls 3rd constructor
-   }
+// //    //second constructor
+// //    public Rectangle(int width, int height){
+// //     this(0,0, width, height); //calls 3rd constructor
+// //    }
 
-public Rectangle(int x, int y, int width, int height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-}
+// // public Rectangle(int x, int y, int width, int height) {
+// //     this.x = x;
+// //     this.y = y;
+// //     this.width = width;
+// //     this.height = height;
+// // }
 
-   //3rd constructor
+//    //3rd constructor
    
-}
+// }
