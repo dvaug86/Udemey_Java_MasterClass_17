8bb6541c1ed1f1dc12bf2563f9fcec15f11b8c1b
David Vaughan <dvaug86@gmail.com>
2024-08-12 09:08:54 -0400

    the 4 ways to instantiate StringBuilder Objects

---
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec99_StringBuilderClass/Main.java
@@ -0,0 +1,21 @@
+package Sections.sec99_StringBuilderClass;
+
+public class Main {
+    public static void main(String[] args) {
+    //4 ways to create a new StringBuilder object using the new keyword:
+    
+    //1. Pass a String
+    StringBuilder helloBuilder = new StringBuilder("Hello");
+
+    //2.Pass no arguments at all
+    StringBuilder emptyBuilder = new StringBuilder();
+
+    //3. Pass an integer value
+    StringBuilder emptyBuilder5 = new StringBuilder(5);
+    
+    //4. Pass some other type of character sequence
+        //below we use the helloBuilder from 1.
+        StringBuilder stringBuilder = new StringBuilder(helloBuilder);
+
+}
+}
