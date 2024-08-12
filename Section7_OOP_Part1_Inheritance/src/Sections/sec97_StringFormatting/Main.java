2fb54fc5e3ca880502d21f74c12d4e8f4419e463
David Vaughan <dvaug86@gmail.com>
2024-08-11 20:54:50 -0400

    comparison methods

--- Section7_OOP_Part1_Inheritance/src/Sections/sec97_StringFormatting/Main.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec97_StringFormatting/Main.java
@@ -2,14 +2,35 @@ package Sections.sec97_StringFormatting;
 
 public class Main {
     public static void main(String[] args) {
-        
-        printInformation("Hello World");
 
+        printInformation("Hello World");
+        printInformation("");
+        printInformation("\t   \n");
+
+        String helloWorld = "Hello World";
+        System.out.printf("index of r = %d %n", helloWorld.indexOf('r'));
+        System.out.printf("index of World = %d %n", helloWorld.indexOf("World"));
+
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l'));
+        System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l'));
+
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l', 3));
+        System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l', 8));
     }
+
     public static void printInformation(String string) {
         int length = string.length();
-        System.out.printf("Length = %d %n", length);;
+        System.out.printf("Length = %d %n", length);
+        if (string.isEmpty()) {
+            System.out.println("String is Empty");
+            return;
+        }
+        if (string.isBlank()) {
+            System.out.println("String is blank");
+        }
         System.out.printf("First char = %c %n", string.charAt(0));
-        //%c just prints a single character
+        // %c just prints a single character
+        System.out.printf("Last char = %c %n", string.charAt(length - 1));
+
     }
 }
