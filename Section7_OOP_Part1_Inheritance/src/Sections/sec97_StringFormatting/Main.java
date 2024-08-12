ab93ef8053530a3f6abe88ef80ca3400cc8114e8
David Vaughan <dvaug86@gmail.com>
2024-08-11 20:37:16 -0400

    string inspection methods

--- Section7_OOP_Part1_Inheritance/src/Sections/sec97_StringFormatting/Main.java
+++ Section7_OOP_Part1_Inheritance/src/Sections/sec97_StringFormatting/Main.java
@@ -3,5 +3,13 @@ package Sections.sec97_StringFormatting;
 public class Main {
     public static void main(String[] args) {
         
+        printInformation("Hello World");
+
+    }
+    public static void printInformation(String string) {
+        int length = string.length();
+        System.out.printf("Length = %d %n", length);;
+        System.out.printf("First char = %c %n", string.charAt(0));
+        //%c just prints a single character
     }
 }
