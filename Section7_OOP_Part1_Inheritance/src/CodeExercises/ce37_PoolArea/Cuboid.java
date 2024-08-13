58a63b06b371a846a81619a1918d25bf0347f534
David Vaughan <dvaug86@gmail.com>
2024-08-13 09:30:17 -0400

    cuboid class complete updated Main now test 1

---
+++ Section7_OOP_Part1_Inheritance/src/CodeExercises/ce37_PoolArea/Cuboid.java
@@ -0,0 +1,22 @@
+package CodeExercises.ce37_PoolArea;
+
+public class Cuboid extends Rectangle {
+
+    // fields
+    private double height;
+
+    // constructors
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height > 0 ? height : 0;
+    }
+
+    // methods
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
+}
--- Section7_OOP_Part1_Inheritance/src/CodeExercises/ce37_PoolArea/Rectangle.java
+++ Section7_OOP_Part1_Inheritance/src/CodeExercises/ce37_PoolArea/Rectangle.java
@@ -1,28 +1,27 @@
 package CodeExercises.ce37_PoolArea;
 
 public class Rectangle {
-//fields
-private double width;
-private double length;
+    // fields
+    private double width;
+    private double length;
 
+    // constructors
+    public Rectangle(double width, double length) {
+        this.width = width > 0 ? width : 0;
+        this.length = length > 0 ? length : 0;
+    }
 
-//constructors
-public Rectangle(double width, double length) {
-    this.width = width > 0 ? width : 0;
-    this.length = length > 0 ? length : 0;
-}
+    // methods
+    public double getWidth() {
+        return width;
+    }
 
-//methods
-public double getWidth(){
-    return width;
-}
+    public double getLength() {
+        return length;
+    }
 
-public double getLength(){
-    return length;
-}
-
-public double getArea(){
-    return length * width;
-}
+    public double getArea() {
+        return length * width;
+    }
 
 }
