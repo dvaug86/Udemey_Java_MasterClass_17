62016d040f54bd080ede3a18f81dac8e36a630d6
David Vaughan <dvaug86@gmail.com>
2024-08-16 15:18:49 -0400

    instructions for challange inputed

--- Section8_OOP_Part2_Polymorphism/src/Sections/sec103_CompositionChallange/Main.java
+++ Section8_OOP_Part2_Polymorphism/src/Sections/sec103_CompositionChallange/Main.java
@@ -36,6 +36,29 @@
  *          brewCoffee()
  * _________________________________________________________________
  * 
+ * addWater() will set the coffee maker's hasWorkToDo field to true
+ * pourMilk() will set the Refrigerator's hasWorkTodDo to true
+ * loadDishwasher() will set the hasWorkToDo flag to true for that appliance.
+ * 
+ * Alternately, you could have a single method called setKitchenState 
+ * that takes three boolean values, which would set each appliance accordingly
+ * 
+ * To execute the work needed to be done by the appliances, you'll implement this in two ways:
+ *      1. your application will access each appliance by using a getter and execute e amethod
+ *          -The apliance methods are orderFood() on refrigerator, doDishes() on dishWasher, 
+ *              and brewCoffee( ) on CoffeeMaker.
+ * 
+ *          -these methods should check the hasWorkToDo flag and if true, print a message out indicating what work is being done
+ * 
+ *      2. Second your application  won't acccess the appliances directly
+ *          -It should call doKitchenWork(), which delegates the work to any of its appliances
+ * 
+ *      
+ * 
+ * 
+ * 
+ * 
+ * 
  */
 
 
