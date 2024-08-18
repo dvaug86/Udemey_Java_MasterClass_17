72e55c63d2d043144e3f69ed459fa2ded3a9fa4f
David Vaughan <dvaug86@gmail.com>
2024-08-17 20:36:41 -0400

    wall class added

---
+++ Section8_OOP_Part2_Polymorphism/src/CodeExercises/ce38_Composition/Wall.java
@@ -0,0 +1,18 @@
+package CodeExercises.ce38_Composition;
+
+public class Wall {
+
+    // fields
+    private String direction;
+
+    // constructor
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    // methods
+    public String getDirection() {
+        return direction;
+    }
+
+}
