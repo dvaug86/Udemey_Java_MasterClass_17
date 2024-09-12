714e1805c24972c387c5ba927579815c9c004999
David Vaughan <dvaug86@gmail.com>
2024-09-12 12:54:25 -0400

    formed all necessary classes for tutorial

---
+++ Section8_OOP_Part2_Polymorphism/src/OtherYoutube/AJ_31_Inheritance/Dog.java
@@ -0,0 +1,5 @@
+package OtherYoutube.AJ_31_Inheritance;
+
+public class Dog {
+
+}
---
+++ Section8_OOP_Part2_Polymorphism/src/OtherYoutube/AJ_32_Composition/Main.java
@@ -0,0 +1,7 @@
+package OtherYoutube.AJ_32_Composition;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
