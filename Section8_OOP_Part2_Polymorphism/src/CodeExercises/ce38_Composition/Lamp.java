2e42fba6e66d18ebdea0d6f483f5f4d898d56c93
David Vaughan <dvaug86@gmail.com>
2024-09-13 08:35:55 -0400

    finished first step with lamp class

--- Section8_OOP_Part2_Polymorphism/src/CodeExercises/ce38_Composition/Lamp.java
+++ Section8_OOP_Part2_Polymorphism/src/CodeExercises/ce38_Composition/Lamp.java
@@ -2,4 +2,34 @@ package CodeExercises.ce38_Composition;
 
 public class Lamp {
 
+    // fields
+    private String style;
+    private boolean battery;
+    private int globRating;
+
+    // constructor
+    public Lamp(String style, boolean battery, int globRating) {
+        this.style = style;
+        this.battery = battery;
+        this.globRating = globRating;
+    }
+
+    // methods
+    // getters
+    public String getStyle() {
+        return style;
+    }
+
+    public int getGlobRating() {
+        return globRating;
+    }
+
+    // other methods
+    public void turnOn() {
+        System.out.println("Lamp is being turned on");
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
 }
