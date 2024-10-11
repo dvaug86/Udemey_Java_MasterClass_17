dcc0cf7e832e4f1932797c8950cc10290d31570b
David Vaughan <dvaug86@gmail.com>
2024-10-11 10:09:56 -0400

    end of part 1 for poly morphism

---
+++ Section8_OOP_Part2_Polymorphism/src/Sections/Section107_Polymorphism_Part1/Main.java
@@ -0,0 +1,12 @@
+package Sections.Section107_Polymorphism_Part1;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        // Movie theMovie = new Movie("Star Wars");
+        // theMovie.watchMovie();
+    
+        Movie theMovie = new Adventure("Star Wars"); //not instead of new Movie it is new Adventure
+        theMovie.watchMovie();
+    }
+}
---
+++ Section8_OOP_Part2_Polymorphism/src/Sections/Section107_Polymorphism_Part1/Movie.java
@@ -0,0 +1,73 @@
+package Sections.Section107_Polymorphism_Part1;
+
+public class Movie {
+
+    // fields
+    private String title;
+
+    // constructor
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie() {
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(title + " is a " + instanceType + " film");
+    }
+}
+
+class Adventure extends Movie {
+
+    // constructor
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        // TODO Auto-generated method stub
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3),
+                "Pleasant Scene",
+                "Scary Music",
+                "Something Bad Happens");
+    }
+
+}
+class Comedy extends Movie {
+    
+    // constructor
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        // TODO Auto-generated method stub
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(4),
+                "Something funny happens",
+                "Something even funnier happens",
+                "Something Bad happens",
+                "Happy ending");
+    }
+
+}
+class ScienceFiction extends Movie {
+    
+    // constructor
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        // TODO Auto-generated method stub
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3),
+                "Bad Aliens do Bad Stuff",
+                "Space Guys Chase Aliens",
+                "Planet blows up");
+    }
+
+}
