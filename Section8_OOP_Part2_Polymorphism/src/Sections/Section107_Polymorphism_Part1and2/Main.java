c463cd2a7d1af3705d7b653abf28de77a73f35ca
David Vaughan <dvaug86@gmail.com>
2024-11-01 08:35:56 -0400

    finished refreshing part 1 of polymorphism

--- Section8_OOP_Part2_Polymorphism/src/Sections/Section107_Polymorphism_Part1and2/Main.java
+++ Section8_OOP_Part2_Polymorphism/src/Sections/Section107_Polymorphism_Part1and2/Main.java
@@ -2,7 +2,7 @@ package Sections.Section107_Polymorphism_Part1and2;
 
 public class Main {
     public static void main(String[] args) {
-        Movie theMovie = new Movie("Star Wars");
+        Movie theMovie = new Adventure("Star Wars");
         theMovie.watchMovie();
     }
 }
--- Section8_OOP_Part2_Polymorphism/src/Sections/Section107_Polymorphism_Part1and2/Movie.java
+++ Section8_OOP_Part2_Polymorphism/src/Sections/Section107_Polymorphism_Part1and2/Movie.java
@@ -1,17 +1,74 @@
 package Sections.Section107_Polymorphism_Part1and2;
 
 public class Movie {
+
+    // fields
     private String title;
 
+    // constructor
     public Movie(String title) {
         this.title = title;
     }
 
+    // methods - this is a public method so it can be called from the outside world.
     public void watchMovie() {
         String instanceType = this.getClass().getSimpleName();
         System.out.println(title + " is a " + instanceType + " film");
     }
+    // .getclass() is a java.lang.object and it returns class type info
 }
- class Adventure extends Movie {
-    
- }
+
+class Adventure extends Movie {
+
+    // constructor
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3),
+                "Pleasant Scene",
+                "Scary Music",
+                "Something Bad Happens");
+    }
+
+}
+
+class Comedy extends Movie {
+
+    // constructor
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(4),
+                "Something funny happens",
+                "something even funnier happens",
+                "something sad happens",
+                "happy ending");
+    }
+
+}
+
+class ScienceFiction extends Movie {
+
+    // constructor
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3),
+                "Bad Aliens do Bad Stuff",
+                "Space Guys Chase Aliens",
+                "Planet Blows up");
+    }
+
+}
